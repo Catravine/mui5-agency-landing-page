25c2cf30d4758d21299b6f821b4f2ecc1e97e58e
Caroline Courtney <caroline.courtney@earthwavetech.com>
2022-11-13 14:59:39 -0500

    basis of navbar, but flex display is weird

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import { Button, Typography, Box } from '@mui/material'
-import CustomButton from './components/Button/';
+import { Box } from '@mui/material'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <Box>
-      <Button variant='contained' color='slateBlue'>Test</Button>
-      <Typography variant='body1'>
-        Hello World!
-      </Typography>
-      <CustomButton>
-        Custom Button
-      </CustomButton>
+      <Navbar />
     </Box>
   );
 }
