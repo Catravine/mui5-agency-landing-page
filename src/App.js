7a35a7743bf101fd97708cf83cb43bd9249efbe2
Caroline Courtney <caroline.courtney@earthwavetech.com>
2022-11-20 16:42:09 -0500

    hero component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Box } from '@mui/material'
-import Header from './components/Header'
+import { Box } from '@mui/material';
+import Header from './components/Header';
+import Hero from './components/Hero';
 
 const App = () => {
   return (
     <Box>
       <Header />
+      <Hero />
     </Box>
   );
 }
